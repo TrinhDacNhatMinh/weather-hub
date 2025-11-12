93125688ef3dc2969415470aaf2b636243fb9bd4
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-11-12 22:43:06 +0700

    feat(auth): add RefreshToken entity

---
+++ src/main/java/com/nhom/weather_hub/entity/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.nhom.weather_hub.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "token", nullable = false, unique = true, length = 500)
+    private String token;
+
+    @Column(name = "expiry_date", nullable = false)
+    private Instant expiryDate;
+
+}
