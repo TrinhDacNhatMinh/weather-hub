3bbdce15b96d5f09380d3af8283d3eb81b0cb072
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-11-12 22:45:23 +0700

    feat(auth): add RefreshTokenRepository interface

--- src/main/java/com/nhom/weather_hub/repository/RefreshTokenRepository.java
+++ src/main/java/com/nhom/weather_hub/repository/RefreshTokenRepository.java
@@ -1,4 +1,17 @@
 package com.nhom.weather_hub.repository;
 
-public interface RefreshTokenRepository {
+import com.nhom.weather_hub.entity.RefreshToken;
+import com.nhom.weather_hub.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    public Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+
 }
