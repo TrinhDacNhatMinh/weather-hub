026c88ef68d30ac8445a91fe92595cc1241b62c6
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-11-19 22:14:43 +0700

    feat(entity): add Station entity

--- src/main/java/com/nhom/weather_hub/entity/Station.java
+++ src/main/java/com/nhom/weather_hub/entity/Station.java
@@ -1,4 +1,52 @@
 package com.nhom.weather_hub.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "stations")
 public class Station {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "latitude", precision = 10, scale = 6)
+    private BigDecimal latitude;
+
+    @Column(name = "longitude", precision = 10, scale = 6)
+    private BigDecimal longitude;
+
+    @Column(name = "api_key", nullable = false, unique = true)
+    private String apiKey;
+
+    @Column(name = "create_at", nullable = false)
+    private Instant createAt;
+
+    @Column(name = "update_at")
+    private Instant updateAt;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
