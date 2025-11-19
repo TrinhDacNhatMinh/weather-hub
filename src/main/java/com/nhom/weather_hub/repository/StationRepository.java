5a82a1e3528e81920b2462a4ca70f9773f7545b5
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-11-19 22:16:34 +0700

    feat(repository): add StationRepository

--- src/main/java/com/nhom/weather_hub/repository/StationRepository.java
+++ src/main/java/com/nhom/weather_hub/repository/StationRepository.java
@@ -1,4 +1,20 @@
 package com.nhom.weather_hub.repository;
 
-public class StationRepository {
+import com.nhom.weather_hub.entity.Station;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StationRepository extends JpaRepository<Station, Long> {
+
+    public Optional<Station> findByApiKey(String apiKey);
+
+    public Page<Station> findByUserId(Long userId, Pageable pageable);
+
+    public boolean existsByApiKey(String apiKey);
+
 }
