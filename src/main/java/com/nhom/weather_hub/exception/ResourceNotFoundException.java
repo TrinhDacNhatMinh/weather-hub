d77e0ef4d9cfd2c3935346d3fb9bc368d56851ac
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-12-08 15:42:42 +0700

    feat(exception): add ResourceNotFoundException

--- src/main/java/com/nhom/weather_hub/exception/ResourceNotFoundException.java
+++ src/main/java/com/nhom/weather_hub/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.nhom.weather_hub.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Object id) {
+        super(resource + " with id " + id + " not found");
+    }
 }
