9028b84334f12f55ad3c3d341b8ec57a1ee66e63
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-12-08 15:44:47 +0700

    feat(security): update SecurityConfig with role-based access for stations and public weather-data POST

--- src/main/java/com/nhom/weather_hub/security/SecurityConfig.java
+++ src/main/java/com/nhom/weather_hub/security/SecurityConfig.java
@@ -5,10 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -34,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
@@ -42,6 +44,12 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/api/auth/**"
+                        ).permitAll()
+                        .requestMatchers(
+                                 "/api/stations/**"
+                        ).hasRole("ADMIN")
+                        .requestMatchers(
+                                HttpMethod.POST, "/api/weather-data/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
