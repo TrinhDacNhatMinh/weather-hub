250e7df9e7b47945fe14630ae29168e753cff87a
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-12-13 10:00:05 +0700

    refactor(exception): improve ResourceNotFoundException

--- src/main/java/com/nhom/weather_hub/exception/ResourceNotFoundException.java
+++ src/main/java/com/nhom/weather_hub/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.nhom.weather_hub.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resource, String findWith, Object o) {
-        super(resource + " with " + findWith + " " + o + " not found");
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
