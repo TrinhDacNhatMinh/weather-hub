9366f8c661a47c2218f80514b95b446215cadaee
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-12-18 00:14:32 +0700

    refactor(event): convert event to records

--- src/main/java/com/nhom/weather_hub/event/AlertCreatedEvent.java
+++ src/main/java/com/nhom/weather_hub/event/AlertCreatedEvent.java
@@ -1,12 +1,9 @@
 package com.nhom.weather_hub.event;
 
 import com.nhom.weather_hub.dto.response.AlertResponse;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public class AlertCreatedEvent {
-    private final Long stationId;
-    private final AlertResponse alertResponse;
+public record AlertCreatedEvent(
+        Long stationId,
+        AlertResponse alertResponse
+) {
 }
--- src/main/java/com/nhom/weather_hub/event/WeatherDataCreatedEvent.java
+++ src/main/java/com/nhom/weather_hub/event/WeatherDataCreatedEvent.java
@@ -1,12 +1,9 @@
 package com.nhom.weather_hub.event;
 
 import com.nhom.weather_hub.dto.response.WeatherDataResponse;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public class WeatherDataCreatedEvent {
-    private final Long stationId;
-    private final WeatherDataResponse weatherDataResponse;
+public record WeatherDataCreatedEvent(
+        Long stationId,
+        WeatherDataResponse weatherDataResponse
+) {
 }
