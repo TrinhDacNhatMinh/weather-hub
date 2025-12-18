ff9d267f66f08b8361744628632f02900f9ab8b8
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-12-18 20:59:10 +0700

    refactor: rename UserNotActiveException to AccountNotActiveException

--- src/main/java/com/nhom/weather_hub/exception/business/AccountNotActiveException.java
+++ src/main/java/com/nhom/weather_hub/exception/business/AccountNotActiveException.java
@@ -3,8 +3,8 @@ package com.nhom.weather_hub.exception.business;
 import com.nhom.weather_hub.exception.base.BusinessException;
 import org.springframework.http.HttpStatus;
 
-public class UserNotActiveException extends BusinessException {
-    public UserNotActiveException() {
+public class AccountNotActiveException extends BusinessException {
+    public AccountNotActiveException() {
         super(HttpStatus.FORBIDDEN, "User is not active");
     }
 }
